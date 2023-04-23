5b7265838b78f045b70eadc94423b0c8c3c90527
Inna Kmit <inna.km7@gmail.com>
2023-04-23 23:43:32 +0100

    adding new doc

--- script6.js
+++ script6.js
@@ -16,16 +16,16 @@
 //   console.log(clients[i]);
 // }
 
-const clients = ["Mango", "Poly", "Ajax"];
-const clientNameToFind = "Poly";
-let message = "Клієнт з таким ім'ям відсутній в базі даних!";
+// const clients = ["Mango", "Poly", "Ajax"];
+// const clientNameToFind = "Poly";
+// let message = "Клієнт з таким ім'ям відсутній в базі даних!";
 
-for (const client of clients) {
-  if (client === clientNameToFind) {
-    message = "Клієнт з таким ім'ям є в базі даних!";
-    break;
-  }
-  // Якщо не збігається, то на цій ітерації нічого не робимо
-}
+// for (const client of clients) {
+//   if (client === clientNameToFind) {
+//     message = "Клієнт з таким ім'ям є в базі даних!";
+//     break;
+//   }
+//   // Якщо не збігається, то на цій ітерації нічого не робимо
+// }
 
 console.log(message); // Клієнт з таким ім'ям є в базі даних!
